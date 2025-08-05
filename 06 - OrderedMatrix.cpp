c7339976f1d3712db832c17359c1769d3d8878e8
mahm0udnasr <mahmoudnasrlite@gmail.com>
2025-08-05 19:41:03 +0300

    solve problem 6

---
+++ 06 - OrderedMatrix.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+void FillMatrixWithOrderNumber(int arr[3][3], short Rows, short Cols) {
+    short counter = 0;
+        for (short i = 0; i < Rows; i++) {
+            for (short j = 0; j < Cols; j++) {
+                counter++;
+                arr[i][j] = counter;
+            }
+        }
+}
+
+void PrintMatrix(int arr[3][3], short Rows, short Cols) {
+    cout << "\n The following is a 3x3 random matrix: \n";
+    for (int i = 0; i < Rows; i++) {
+        for (int j = 0; j < Cols; j++) {
+            cout << setw(3) << arr[i][j] << "    ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int arr[3][3];
+    FillMatrixWithOrderNumber(arr, 3, 3);
+    PrintMatrix(arr, 3, 3);
+}
