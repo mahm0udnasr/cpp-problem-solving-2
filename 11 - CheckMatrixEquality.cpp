3ec0a5cd937cbebe3da7ba0ad8d45e608fbd2d2a
mahm0udnasr <mahmoudnasrlite@gmail.com>
2025-08-07 07:32:50 +0300

    solve problem 11

---
+++ 11 - CheckMatrixEquality.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
+
+using namespace std;
+
+int RandomNumber(int From, int To) {
+    return rand() % To + From;
+}
+
+void FillMatrixWithRandomNumber(int arr[3][3], short Rows, short Cols) {
+    for (int i = 0; i < Rows; i++) {
+        for (int j = 0; j < Cols; j++) {
+            arr[i][j] = RandomNumber(1, 100);
+        }
+    }
+}
+
+void PrintMatrix(int arr[3][3], short Rows, short Cols) {
+    for (int i = 0; i < Rows; i++) {
+        for (int j = 0; j < Cols; j++) {
+            cout << setw(3) << arr[i][j] << "    ";
+        }
+        cout << endl;
+    }
+}
+
+bool CheckMatricesEquality(int arr1[3][3], int arr2[3][3], short Rows, short Cols) {
+    for (int i = 0; i < Rows; i++) {
+        for (int j = 0; j < Cols; j++) {
+            if (arr1[i][j] != arr2[i][j]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+
+
+int main()
+{
+    srand((unsigned)time(NULL));
+    int arr1[3][3], arr2[3][3];
+
+    FillMatrixWithRandomNumber(arr1, 3, 3);
+    FillMatrixWithRandomNumber(arr2, 3, 3);
+
+    cout << "\nMatrix 1 : \n";
+    PrintMatrix(arr1, 3, 3);
+    cout << "\nMatrix 2 : \n";
+    PrintMatrix(arr2, 3, 3);
+
+    cout << endl << (CheckMatricesEquality(arr1, arr2, 3, 3) ? "Yes" : "No") << " : Matrices are " << (CheckMatricesEquality(arr1, arr2, 3, 3) ? "Equal" : "Not Equal") << "." << endl;
+}
